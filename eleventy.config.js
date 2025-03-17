de1472df71849acf1a6ba89312c811b521d5bc12
Luis Saturno <saturno.luis@gmail.com>
2025-03-18 00:14:12 +0100

    add icons and buttons

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,11 @@
+export default async function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy('static/fonts');
+	eleventyConfig.addPassthroughCopy('static/css');
+
+	eleventyConfig.addPassthroughCopy('CNAME');
+	eleventyConfig.addPassthroughCopy('robots.txt');
+}
+
 export const config = {
 	dir: {
 		input: "content",
