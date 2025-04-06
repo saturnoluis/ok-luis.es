831c4e0060dea94de65e0f219d6ad3a36d2e6361
Luis Saturno <saturno.luis@gmail.com>
2025-04-06 22:12:34 +0200

    Improve scripts

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
 		},
 	],
 	webServer: {
-		command: 'npm run build && npm run start',
+		command: 'npm run build && npm run serve',
 		url: 'http://127.0.0.1:8080',
 	},
 });
