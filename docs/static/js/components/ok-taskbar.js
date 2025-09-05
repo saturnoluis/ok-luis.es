6cddcaa4ff63587719e7cffacba646f710825f33
Luis Saturno <saturno.luis@gmail.com>
2025-09-05 17:49:30 +0200

    run build

---
+++ docs/static/js/components/ok-taskbar.js
@@ -0,0 +1,99 @@
+import { LitElement, html, css } from '/static/js/dependencies/lit-all.min.js';
+import { toggleClassName } from '/static/js/utils.js';
+
+class OkTaskbar extends LitElement {
+	static properties = {
+		taskButton: {
+			type: String,
+			attribute: 'task-button',
+		},
+	};
+
+	static styles = getStyles();
+
+	constructor() {
+		super();
+	}
+
+	firstUpdated() {
+		super.connectedCallback();
+
+		if (this.hasAttribute('task-button')) {
+			const taskButtonContent = this.getAttribute('task-button');
+
+			if (taskButtonContent.length > 0) {
+				const taskButton = this.renderRoot.getElementById('task-button');
+				toggleClassName(taskButton, 'display-block');
+			}
+		}
+	}
+
+	_handleClick(event) {
+		this.dispatchEvent(new CustomEvent('ok-task-button-click'));
+	}
+
+	render() {
+		return html`
+			<div class="inner">
+				<button
+					id="task-button"
+					class="task-button"
+					@click="${this._handleClick}"
+				>
+					${this.taskButton}
+				</button>
+			</div>
+		`;
+	}
+}
+
+customElements.define('ok-taskbar', OkTaskbar);
+
+function getStyles() {
+	return css`
+		:host {
+			display: block;
+			width: 100%;
+			height: var(--taskbar-height);
+			box-sizing: border-box;
+			background: var(--surface);
+			box-shadow:
+				inset -1rem -1rem var(--surface-shadow-darker),
+				inset 1rem 1rem var(--surface-glow);
+		}
+
+		.inner {
+			display: flex;
+			align-content: center;
+			padding: 2rem;
+			box-sizing: border-box;
+			height: 100%;
+		}
+
+		#task-button {
+			display: none;
+			color: var(--text);
+			background: var(--surface);
+			font-family: 'Sysfont', sans-serif;
+			font-size: 9rem;
+			padding: 0 8rem;
+			cursor: pointer;
+			box-sizing: border-box;
+			border: none;
+			box-shadow:
+				inset -1rem -1rem var(--surface-shadow-darker),
+				inset 1rem 1rem var(--surface-glow);
+		}
+
+		#task-button.display-block {
+			display: block;
+		}
+
+		#task-button:active {
+			background-color: var(--surface-shadow);
+			box-shadow:
+				inset 1rem 1rem var(--surface-shadow-darker),
+				inset -1rem -1rem var(--surface-glow);
+		}
+	`;
+}
---
+++ docs/static/js/constants.js
@@ -0,0 +1,20 @@
+export const desktopIcons = [
+	{
+		href: '/quien-soy',
+		img: '/static/img/icon-about-me.png',
+		alt: '¿Quién soy?',
+		label: '¿Quién soy?',
+	},
+	{
+		href: '/articles',
+		img: '/static/img/icon-articles.png',
+		alt: 'Artículos',
+		label: 'Artículos',
+	},
+	{
+		href: '/my-links',
+		img: '/static/img/icon-my-links.png',
+		alt: 'Enlaces',
+		label: 'Enlaces',
+	},
+];
